59d1c7e04e286191fefe594c394b1fcc2a41290f
Pranav Prashob <pranavprashob02@gmail.com>
2022-07-18 00:56:22 +0530

    18-07-2022

--- Java Programs/MathUtils/src/com/pranav/calculator/MathUtils.java
+++ Java Programs/MathUtils/src/com/pranav/calculator/MathUtils.java
@@ -6,4 +6,20 @@ public class MathUtils {
 		return a+b;
 	}
 	
+	public int subract(int a, int b) {
+		return a-b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
+	public double computeCircleArea(double r) {
+		return Math.PI*r*r;
+	}
+	
 }
--- Java Programs/MathUtils/src/com/pranav/test/MathUtilsTest.java
+++ Java Programs/MathUtils/src/com/pranav/test/MathUtilsTest.java
@@ -2,15 +2,63 @@ package com.pranav.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import com.pranav.calculator.MathUtils;
 
-class MathUtilsTest {
+/*
+ * If I use @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+ * Then I won't have to specify the @BeforeAll and @AfterAll hook annotations as static.
+ * As only one instance will be created instead of one for each method.
+ */
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)		//	Only one instance per class instead of one per method.
+class MathUtilsTest {
+	
+	MathUtils mathUtils;
+	
+	/*
+	 * Four types of hook annotations:
+	 * 	1. @BeforeAll : Executed before any test runs, has to be a static method as JUnit creates 
+	 * 					instance every single time a test runs, as this runs before any tests, no 
+	 * 					instance is created and hence can't be a non-static function.
+	 * 	
+	 * 	2. @AfterAll : Executed After all tests are run, has to be a static method (same reason as above).
+	 * 	
+	 * 	3. @BeforeEach : Executed before running every single test.
+	 * 
+	 * 	4. @AfterEach : Executed after running every single test.
+	 */
+	
+//	@BeforeAll
+//	static void beforeAllInit() {
+//		System.out.println("This needs to be run before all.");
+//	}
+	
+	@BeforeAll
+	void beforeAllInit() {
+		System.out.println("This needs to be run before all.");
+	}
+	
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+
+	@AfterEach
+	void cleaup() {
+		System.out.println("Cleaning up..:)");
+	}
+	
 	@Test
 	final void should_check_if_addition_is_correct() {
-		MathUtils mathUtils = new MathUtils();
+//		MathUtils mathUtils = new MathUtils();
 		
 //		Given:
 		int expected, a, b;
@@ -25,5 +73,21 @@ class MathUtilsTest {
 		assertEquals(expected, exp_result, "This method should add two numbers");
 		
 	}
+	
+	@Test
+	final void should_verify_cicle_radius() {
+//		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return the correct circle area");
+	}
+	
+	@Test
+	final void should_throw_exception_when_divide_by_zero() {
+//		MathUtils mathUtils = new MathUtils();
+//		assertThrows(NullPointerException.class, ()-> mathUtils.divide(1, 0), "Divide by zero should throw exception");
+
+		assertThrows(ArithmeticException.class, ()-> mathUtils.divide(1, 0), "Divide by zero should throw exception");
+		
+	}
+	
 
 }
